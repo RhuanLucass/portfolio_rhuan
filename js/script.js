a4fd4ee3322572c8740313d8bb1ee24dfc7667c0
Rhuan <rhuanfernandes.rf@gmail.com>
2024-10-23 21:47:58 -0300

    feat: header adjustments and menu mobile with transitions

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ scrollHeader = () => {
 }
 
 window.addEventListener('scroll', scrollHeader)
+
+// Toggle menu on mobile
+const buttonMenuMobile = document.querySelector('.menu-mobile')
+toggleMenu = () => {
+  const span = buttonMenuMobile.querySelector('span')
+  const menuMobile = document.querySelector('.menu-desktop')
+
+  menuMobile.classList.toggle('active-menu-mobile')
+  span.classList.toggle('toggle-button-menu')
+}
+
+buttonMenuMobile.addEventListener('click', toggleMenu)
